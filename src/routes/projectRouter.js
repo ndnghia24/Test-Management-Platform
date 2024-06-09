f91b4ba7b466979b92329a7fe301360da3bb2820
ndnghia24 <91201271+ndnghia24@users.noreply.github.com>
2024-06-09 22:47:57 +0700

    Update projectRouter.js

--- src/routes/projectRouter.js
+++ src/routes/projectRouter.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const controller = require("../controllers/projectController");
 
 //PROJECT SUPPORT ROUTES
-router.get("/:id/getAllModule", controller.getModule);
 router.get("/:id/getAllRequirement", controller.getRequirement);
 
 //TESTPLAN
@@ -20,12 +19,25 @@ router.delete("/:id/testcase/deleteTestCase", controller.test_caseController.del
 
 //MODULE
 router.get("/:id/module", controller.moduleController.getModule);
+router.get("/:id/module/getModule", controller.moduleController.getAllModule);
+router.post("/:id/module/addModule", controller.moduleController.addModule);
 
-//REQUIREMENT
+//REQUIREMENT & REQUIREMENT TYPE
 router.get("/:id/requirement", controller.requirementController.getRequirement);
+router.get("/:id/requirement/getRequirement", controller.requirementController.getSpecifyRequirement);
+router.get("/:id/requirement/getRequirementByTypeFilter", controller.requirementController.getRequirementByTypeFilter);
+router.post("/:id/requirement/addRequirement", controller.requirementController.addRequirement);
+router.put("/:id/requirement/editRequirement", controller.requirementController.editRequirement);
+router.delete("/:id/requirement/deleteRequirement", controller.requirementController.deleteRequirement);
+
+router.get("/:id/getRequirementType", controller.requirementController.getRequirementType);
+router.post("/:id/addRequirementType", controller.requirementController.addRequirementType);
 
 //RELEASE
 router.get("/:id/release", controller.releaseController.getRelease);
+router.post("/:id/release/addRelease", controller.releaseController.addRelease);
+router.put("/:id/release/editRelease", controller.releaseController.editRelease);
+router.delete("/:id/release/deleteRelease", controller.releaseController.deleteRelease);
 
 //OVERVIEW
 router.get("/:id/overview", controller.overviewController.getOverview);
@@ -36,8 +48,8 @@ router.get("/:id/report", controller.reportController.getReport);
 
 // router.put("/:id/testcase/editTestCaseLinking", controller.test_caseController.editTestCase);
 // router.put("/:id/testcase/editTestCaseRequirement", controller.test_caseController.editTestCase);
-router.put("/:id/testcase/editTestCaseStep", controller.test_caseController.editTestCaseStep);
-router.put("/:id/testcase/editTestCaseOverview", controller.test_caseController.editTestCaseOverview);
+// router.put("/:id/testcase/editTestCaseStep", controller.test_caseController.editTestCase);
+// router.put("/:id/testcase/editTestCaseOverview", controller.test_caseController.editTestCase);
 
 router.get("/:id/testrun", (req,res) => {
     res.render('test-run-view', {
