e5c725c5786e84f3abf56d4e28f4d01dd7c3edd5
BaoNinh <4legsmonster@gmail.com>
2024-06-10 12:48:44 +0700

    complete all filter logic and link

--- public/js/issueButton.js
+++ public/js/issueButton.js
@@ -18,3 +18,50 @@ async function goToIssueDetail(element) {
         console.error('Failed to update the issue');
     }
 }
+
+async function sendFilterRequest(element){
+    const projectId = parseInt(element.dataset.projectId);
+    
+    const statusDropdown = document.getElementById('status-filter');
+    const status = statusDropdown.options[statusDropdown.selectedIndex].value;
+
+    const priorityDropdown = document.getElementById('priority-filter');
+    const priority = priorityDropdown.options[priorityDropdown.selectedIndex].value;
+
+    const assignedToDropdown = document.getElementById('assignedTo-filter');
+    const assignedTo = assignedToDropdown.options[assignedToDropdown.selectedIndex].value;
+
+    const createdByDropdown = document.getElementById('createdBy-filter');
+    const createdBy = createdByDropdown.options[createdByDropdown.selectedIndex].value;
+
+    let query = '';
+    if(status){
+        query += `status=${status}&`;
+    }
+    if(priority){
+        query += `priority=${priority}&`;
+    }
+    if(createdBy){
+        query += `createdBy=${createdBy}&`;
+    }
+    if(assignedTo){
+        query += `assignedTo=${assignedTo}&`;
+    }
+
+    // Send a GET request with the query string
+    const response = await fetch(`/project/${projectId}/issues?${query}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    });
+
+    // Check if the request was successful
+    if (response.ok) {
+        // Redirect to the issue page
+        window.location.href = `/project/${projectId}/issues?${query}`;
+    } else {
+        // Handle errors, if any
+        console.error('Failed to update the issue');
+    }
+}
