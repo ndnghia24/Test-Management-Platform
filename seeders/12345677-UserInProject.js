795aabc783716ed21d64fe6c02aa778cc377e007
Thanh-Nghi Do <21120292@student.hcmus.edu.vn>
2024-07-14 11:57:54 +0700

    Xinh dep tuyet voi 2

--- seeders/12345677-UserInProject.js
+++ seeders/12345677-UserInProject.js
@@ -4,8 +4,8 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.bulkInsert('user_in_project', [
-      { user_id: 1, project_id: 1, role_id: 1, is_delete: false, createdAt: new Date(), updatedAt: new Date()},
-      { user_id: 2, project_id: 1, role_id: 2, is_delete: false, createdAt: new Date(), updatedAt: new Date()},
+      { user_id: 1, project_id: 1, role_id: 2, is_delete: false, createdAt: new Date(), updatedAt: new Date()},
+      { user_id: 2, project_id: 1, role_id: 1, is_delete: false, createdAt: new Date(), updatedAt: new Date()},
       { user_id: 3, project_id: 1, role_id: 3, is_delete: false, createdAt: new Date(), updatedAt: new Date()},
     ], {});
   },
